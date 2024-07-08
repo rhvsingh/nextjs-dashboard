f6c1179d3527c7a11c35f066938ed9acd3f6db79
Raja Harsh Vardhan Singh <singhrajaharshvardhan004@gmail.com>
2024-07-09 03:46:36 +0530

    feat: added auth

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,6 +7,12 @@ import { Suspense } from "react"
 import { CardSkeleton, RevenueChartSkeleton, LatestInvoicesSkeleton } from "@/app/ui/skeletons"
 import CardWrapper from "@/app/ui/dashboard/cards"
 
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Dashboard",
+}
+
 export default async function Page() {
     return (
         <main>
